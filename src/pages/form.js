bc9e9d8d18b8af87de96987de7223cc4f2705386
Kanchai Lerdsrisakulrat <kanchai.lerd@kmutt.ac.th>
2025-03-09 05:40:05 +0700

    Update Form.css

--- src/pages/form.js
+++ src/pages/form.js
@@ -5,6 +5,18 @@ const RegisterForm = () => {
     return(
         <div>
             <Navbar/>
+            <div className='container'>
+                <div className='content-text'>
+                    <h4>Enter Your</h4>
+                    <h4>Information To</h4>
+                    <h3>Get A Special Deal</h3>
+                </div>
+                <div className='content-form'>
+                    <h4>Enter Your</h4>
+                    <h4>Information To</h4>
+                    <h3>Get A Special Deal</h3>
+                </div>
+            </div>
         </div>
     )
 }
